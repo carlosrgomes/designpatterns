33cd5628f1d90e14be573f49bb6e3b8a161c706c
carlosgomes@gmail.com <carlosgomes@gmail.com>
2014-10-14 09:01:35 -0300

    adicionando strategy

--- strategy/src/main/java/br/com/barbero/Context.java
+++ strategy/src/main/java/br/com/barbero/Context.java
@@ -4,5 +4,14 @@ package br.com.barbero;
  * Created by prcarlosbarbero on 10/13/14.
  */
 public class Context {
+
     private Strategy strategy;
+
+    public Strategy getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
 }
